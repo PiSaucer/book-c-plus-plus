78b2cf7abefd6535e85f04bf4198dee3fd006164
PiSaucer <PiSaucer@gmail.com>
2020-07-27 09:59:45 -0500

    GuessMyNumber

--- chapter2/GuessMyNumber/GuessMyNumber.cpp
+++ chapter2/GuessMyNumber/GuessMyNumber.cpp
@@ -1,20 +1,42 @@
-// GuessMyNumber.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// Guess My Number
+// The classic number guessing game
 
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    srand(static_cast<unsigned int>(time(0))); //seed random number generator
+
+    int secretNumber = rand() % 100 + 1; // random number between 1 and 100
+    int tries = 0;
+    int guess;
+
+    cout << "\tWelcome to Guess My Number\n\n";
+
+    do 
+    {
+        cout << "Enter a guess: ";
+        cin >> guess;
+        ++tries;
+
+        if (guess > secretNumber)
+        {
+            cout << "Too high!\n\n";
+        } else if (guess < secretNumber)
+        {
+            cout << "Too low!\n\n";
+        }
+        else 
+        {
+            cout << "\nThat's it! You got it in " << tries << " guesses!\n";
+        }
+    } while (guess != secretNumber);
+
+return 0;
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
---
+++ chapter2/GuessMyNumber/run.bat
@@ -0,0 +1,2 @@
+GuessMyNumber.exe
+pause
