5866fabb0401d44ffc82ebdb46bda52980592ba4
PiSaucer <pisaucer@gmail.com>
2020-08-16 08:46:43 -1000

    Create Pointing.cpp

---
+++ chapter7/Pointing/Pointing.cpp
@@ -0,0 +1,19 @@
+// Pointing
+// Demonstrates using pointers
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int* pAPointer; //declare a pointer
+
+    int* pScore = 0; //declare and initialize a pointer
+
+    int score = 1000;
+    pScore = &score; //assign pointer pScore address of variable score
+
+    
+}
