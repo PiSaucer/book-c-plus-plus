6448a61b588be767071440f258d3d79c74034eaa
PiSaucer <PiSaucer@gmail.com>
2020-08-16 16:55:54 -0500

    Pointing

--- chapter7/Pointing/Pointing.cpp
+++ chapter7/Pointing/Pointing.cpp
@@ -20,4 +20,32 @@ int main()
     cout << "pScore is: " << pScore << "\n"; //address of stored in pointer
     cout << "score is :" << score << "\n"; 
     cout << "*pScore is: " << *pScore << "\n\n"; //value pointed to by pointer
+
+    cout << "Adding 500 to score\n";
+    score += 500;
+    cout << "score is: " << score << "\n";
+    cout << "*pScore is: " << *pScore << "\n\n";
+
+    cout << "Adding 500 to *pScore\n";
+    *pScore += 500;
+    cout << "score is: " << score << "\n";
+    cout << "*pScore is: " << *pScore << "\n\n";
+
+    cout << "Assigning &newScore to pScore\n";
+    int newScore = 5000;
+    pScore = &newScore;
+    cout << "&newScore is: " << &newScore << "\n";
+    cout << "pScore is: " << pScore << "\n";
+    cout << "newScore is: " << newScore << "\n";
+    cout << "*pScore is: " << *pScore << "\n\n";
+
+    cout << "Assigning &str to pStr\n";
+    string str = "score";
+    string* pStr = &str; //pointer to string object
+    cout << "str is: " << str << "\n";
+    cout << "*pStr is: " << *pStr << "\n";
+    cout << "(*pStr).size() is: " << (*pStr).size() << "\n";
+    cout << "pStr->size() is: " << pStr->size() << "\n";
+
+    return 0;
 }
---
+++ chapter7/Pointing/run.bat
@@ -0,0 +1,2 @@
+Pointing.exe
+pause
